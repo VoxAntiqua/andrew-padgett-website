0698fbf34a3e8fa3fef266c7c44c14dbd75592b3
VoxAntiqua <andrew.padgett.88@gmail.com>
2024-08-31 18:52:04 -0400

    Implemented event chronological sorting

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -7,14 +7,21 @@ import EventsList from "../ui/calendar/EventsList";
 import EventDetails from "../ui/calendar/EventDetails";
 
 export default function Calendar() {
-  const [selectedEvent, setSelectedEvent] = useState<Event | null>(events[0]);
+  // Sort the events array by the first date in ascending order
+  const sortedEvents = [...events].sort((a, b) => {
+    const firstDateA = new Date(a.timesLocations[0].time);
+    const firstDateB = new Date(b.timesLocations[0].time);
+    return firstDateA.getTime() - firstDateB.getTime();
+  });
+
+  const [selectedEvent, setSelectedEvent] = useState<Event | null>(sortedEvents[0]);
 
   return (
     <section className="p-8 lg:px-16">
       <div className="container">
         <div className="flex justify-center items-start p-8">
           {/* Left Side: Events List */}
-          <EventsList events={events} onSelectEvent={setSelectedEvent} selectedEvent={selectedEvent} />
+          <EventsList events={sortedEvents} onSelectEvent={setSelectedEvent} selectedEvent={selectedEvent} />
 
           {/* Vertical Divider */}
           <div className="border-l border-gray-300 mx-8 h-auto self-stretch"></div>
