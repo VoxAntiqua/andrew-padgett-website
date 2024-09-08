d1a4152d5f8c565b30ece8fe30330be433adde6f
VoxAntiqua <andrew.padgett.88@gmail.com>
2024-09-07 20:41:26 -0400

    Fixed unescaped characters in press page

--- app/press/page.tsx
+++ app/press/page.tsx
@@ -7,31 +7,31 @@ const Press: React.FC = () => {
         <div className="flex-1 space-y-8">
           {/* Column 1 */}
           <p>
-            “Similarly, the bass of Andrew Padgett shines in the next number. The details of this penultimate offering are as speculative as they are complex: The melody for the contrafactum Entre.l Taur e.l Doble Signe is from Bertran de Born’s Rassa tan creis via Monge de Montaudan’s Mot m’envaya, while the text itself has enjoyed a variety of attributions. It is, however, impossibly lovely, a celebration for the season “between Taurus and Gemini,” as the text puts it (Spring, in other words). Padgett throws himself into the song, ably supported by Seligmann.”
+            &quot;Similarly, the bass of Andrew Padgett shines in the next number. The details of this penultimate offering are as speculative as they are complex: The melody for the contrafactum Entre.l Taur e.l Doble Signe is from Bertran de Born&apos;s Rassa tan creis via Monge de Montaudan&apos;s Mot m&apos;envaya, while the text itself has enjoyed a variety of attributions. It is, however, impossibly lovely, a celebration for the season &quot;between Taurus and Gemini,&quot; as the text puts it (Spring, in other words). Padgett throws himself into the song, ably supported by Seligmann.&quot;
             <br />
             <div className='text-right py-3'>Colin Clarke, <a href="https://fanfarearchive.com/ntbm/articles/thompson-clarke.html" target="_blank" rel="noopener noreferrer">Fanfare Archive</a></div>
           </p>
 
           <p>
-            “Players and soloists delivered their usual fine musicianship throughout: Sopranos Corinne Byrne and Sonja Tengblad, alto Carrie Cheron, tenors Matthew Anderson and Charles Blandy, and bass Andrew Padgett all brought their excellent diction and heart to the work. Standing out was Heidi Braun-Hill, concert mistress, who brought out the plain tune within Bach’s ornamental string writing in her duet with tenor Blandy. Here, Rafael Popper-Keiser and Michael Beattie deserve recognition for their always expert and compassionate basso continuo. The double reed quartet of Peggy Pearson, Catherine Weinfeld-Zell, and Jennifer Slowik (on English horn) and bassoonist Adrian Morejon, with bass Andrew Padgett in the instrumental chorale “Why are you so angry?” was a brilliant contrast between slogging through “earth and dung” and harried confusion. Following this was a heartbreaking double duet with Sonja Tengblad’s rich soprano and Carrie Cheron’s warm alto, while Jennifer Slowik on English horn duetted with flautist Vanessa Holroyd. The final chorale for this gigantic work was bare bones, making an incredibly surprising and perfect contrast.”
+            &quot;Players and soloists delivered their usual fine musicianship throughout: Sopranos Corinne Byrne and Sonja Tengblad, alto Carrie Cheron, tenors Matthew Anderson and Charles Blandy, and bass Andrew Padgett all brought their excellent diction and heart to the work. Standing out was Heidi Braun-Hill, concert mistress, who brought out the plain tune within Bach&apos;s ornamental string writing in her duet with tenor Blandy. Here, Rafael Popper-Keiser and Michael Beattie deserve recognition for their always expert and compassionate basso continuo. The double reed quartet of Peggy Pearson, Catherine Weinfeld-Zell, and Jennifer Slowik (on English horn) and bassoonist Adrian Morejon, with bass Andrew Padgett in the instrumental chorale &quot;Why are you so angry?&quot; was a brilliant contrast between slogging through &quot;earth and dung&quot; and harried confusion. Following this was a heartbreaking double duet with Sonja Tengblad&apos;s rich soprano and Carrie Cheron&apos;s warm alto, while Jennifer Slowik on English horn duetted with flautist Vanessa Holroyd. The final chorale for this gigantic work was bare bones, making an incredibly surprising and perfect contrast.&quot;
             <br />
             <div className='text-right py-3'>Amelia LeClair, <a href="https://www.classical-scene.com/2024/06/07/carrying-bach/" target="_blank" rel="noopener noreferrer">Boston Music Intelligencer</a></div>
           </p>
 
           <p>
-            “Kyle Stegall, tenor, Andrew Padgett, bass, and John Taylor Ward, bass, were particularly outstanding.”
+            &quot;Kyle Stegall, tenor, Andrew Padgett, bass, and John Taylor Ward, bass, were particularly outstanding.&quot;
             <br />
             <div className='text-right py-3'>David Griesinger, <a href="https://www.classical-scene.com/2012/11/13/wonder-suzuki/" target="_blank" rel="noopener noreferrer">Boston Music Intelligencer</a></div>
           </p>
 
           <p>
-            “In last year’s El Mesías, Andrew Padgett sang with the bass section, but this year as soloist he employed his commanding bass baritone to give illuminating definition to the haunting aria “El pueblo que estaba en tinieblas” (The people that walked in darkness).”
+            &quot;In last year&apos;s El Mesías, Andrew Padgett sang with the bass section, but this year as soloist he employed his commanding bass baritone to give illuminating definition to the haunting aria &quot;El pueblo que estaba en tinieblas&quot; (The people that walked in darkness).&quot;
             <br />
             <div className='text-right py-3'>Ken Herman, <a href="https://sandiegostory.com/bach-collegium-san-diegos-transcendent-el-mesias-in-point-loma/" target="_blank" rel="noopener noreferrer">San Diego Story</a></div>
           </p>
 
           <p>
-            “...By contrast, The Lord will surely come starts with a soothing ‘Kyrie’ before becoming increasingly rhapsodic and Howellsian. Andrew Padgett’s gripping baritone solo is another highlight.”
+            &quot;...By contrast, The Lord will surely come starts with a soothing &apos;Kyrie&apos; before becoming increasingly rhapsodic and Howellsian. Andrew Padgett&apos;s gripping baritone solo is another highlight.&quot;
             <br />
             <div className='text-right py-3'>Malcolm Riley, Gramophone</div>
           </p>
@@ -40,43 +40,43 @@ const Press: React.FC = () => {
         <div className="flex-1 space-y-8">
           {/* Column 2 */}
           <p>
-            “...Equally effective is the accumulating interest of ‘The Lord Will Surely Come’, an anthem that has deservedly earned its place within the American church. Baritone Andrew Padgett is splendidly declamatory in his brief solo, but it is the work’s exotic harmonic language (now prominently Gallic) and its melodic ‘rolling breakers’ that leave no doubt of a second coming implied by the work’s title.”
+            &quot;...Equally effective is the accumulating interest of &apos;The Lord Will Surely Come&apos;, an anthem that has deservedly earned its place within the American church. Baritone Andrew Padgett is splendidly declamatory in his brief solo, but it is the work&apos;s exotic harmonic language (now prominently Gallic) and its melodic &apos;rolling breakers&apos; that leave no doubt of a second coming implied by the work&apos;s title.&quot;
             <br />
             <div className='text-right py-3'>David Truslove, <a href="https://operatoday.com/2021/06/the-music-of-celebrated-american-organist-choirmaster-gerre-hancock/" target="_blank" rel="noopener noreferrer">Opera Today</a></div>
           </p>
 
           <p>
-            “The men of the choir are at their most suave in the plainsong quoting “Creator of the Stars of night,” with a very fine solo from baritone Andrew Padgett.”
+            &quot;The men of the choir are at their most suave in the plainsong quoting &quot;Creator of the Stars of night,&quot; with a very fine solo from baritone Andrew Padgett.&quot;
             <br />
             <div className='text-right py-3'>Geoffrey Williams, <a href="https://livingchurch.org/2021/05/10/a-masterly-tribute-to-gerre-hancock/" target="_blank" rel="noopener noreferrer">The Living Church</a></div>
           </p>
 
           <p>
-            “Eight singers took on Letter to My Father (in three parts), the fourth work in contemporary American composer Ted Hearne’s Coloring Book. The text was by Zora Neal Hurston—“How does it feel to be colored.” It was pulled apart into repeated words and phrases, with perfectly tuned dissonances and odd rhythms. Abrupt phrases were contrasted with long, long lines; Andrew Padgett’s clear bass-baritone invested a crucial line with telling import.”
+            &quot;Eight singers took on Letter to My Father (in three parts), the fourth work in contemporary American composer Ted Hearne&apos;s Coloring Book. The text was by Zora Neal Hurston—&quot;How does it feel to be colored.&quot; It was pulled apart into repeated words and phrases, with perfectly tuned dissonances and odd rhythms. Abrupt phrases were contrasted with long, long lines; Andrew Padgett&apos;s clear bass-baritone invested a crucial line with telling import.&quot;
             <br />
             <div className='text-right py-3'>John Campbell, <a href="https://www.artsongupdate.org/Reviews/VWC/VWC19.htm" target="_blank" rel="noopener noreferrer">Artsong Update</a></div>
           </p>
 
           <p>
-            “Soprano Laura Heimes has been a much-loved soloist with Pegasus for some time now, and she was joined by three equally impressive colleagues. Alto Luthien Brackett brought out all the drama of Strozzi's short biblical cantata, "In Medio Maris," describing Jesus's walk on the Sea of Galilee and Peter's less successful follow-up. Tenor Andrew Fuchs' bright, open tenor voice is ideal for this repertoire, and Andrew Padgett provided that dark, precisely focused bass voice sound that seems unique to early music. Individually outstanding, the four singers blended amazingly well as a quartet, and offered a couple of delightful amorous duets along the way (the texts of many of Strozzi's vocal works were by her father, and they're quite spicy, in a literary way).”
+            &quot;Soprano Laura Heimes has been a much-loved soloist with Pegasus for some time now, and she was joined by three equally impressive colleagues. Alto Luthien Brackett brought out all the drama of Strozzi's short biblical cantata, "In Medio Maris," describing Jesus's walk on the Sea of Galilee and Peter's less successful follow-up. Tenor Andrew Fuchs' bright, open tenor voice is ideal for this repertoire, and Andrew Padgett provided that dark, precisely focused bass voice sound that seems unique to early music. Individually outstanding, the four singers blended amazingly well as a quartet, and offered a couple of delightful amorous duets along the way (the texts of many of Strozzi's vocal works were by her father, and they're quite spicy, in a literary way).&quot;
             <br />
             <div className='text-right py-3'>David Raymond, <a href="https://www.rochestercitynewspaper.com/rochester/classical-review-barbaras-venice/Content?oid=2890463" target="_blank" rel="noopener noreferrer">Rochester City Newspaper</a></div>
           </p>
 
           <p>
-            “The two bass-baritones — John Taylor Ward as Manoa, Samson’s father, and Andrew Padgett as the Philistine warrior Harapha — sang well.”
+            &quot;The two bass-baritones — John Taylor Ward as Manoa, Samson&apos;s father, and Andrew Padgett as the Philistine warrior Harapha — sang well.&quot;
             <br />
             <div className='text-right py-3'>James Oestreich, <a href="https://www.nytimes.com/2014/03/06/arts/music/american-classical-orchestra-performs-handels-samson.html" target="_blank" rel="noopener noreferrer">NY Times</a></div>
           </p>
 
           <p>
-            “Andrew Padgett as Harapha gave a fine performance and excelled in the aria, “Honour and arms scorn such a foe.””
+            &quot;Andrew Padgett as Harapha gave a fine performance and excelled in the aria, &quot;Honour and arms scorn such a foe.&quot;&quot;
             <br />
             <div className='text-right py-3'>Stan Metzger, <a href="https://seenandheard-international.com/2014/03/handels-samson-larger-life/" target="_blank" rel="noopener noreferrer">Seen and Heard International</a></div>
           </p>
 
           <p>
-            “Andrew Padgett set down his hurdy-gurdy to sing bass in liquor-themed works of Purcell and Antonio Cesti. The former’s “Bacchus is a Pow’r Divine” showcased Padgett’s powerful baritone and impressive vocal range; a natural actor, he stumbled and hiccupped convincingly through the night’s many entreaties on the importance of good wine without overdoing the gestures.”
+            &quot;Andrew Padgett set down his hurdy-gurdy to sing bass in liquor-themed works of Purcell and Antonio Cesti. The former&apos;s &quot;Bacchus is a Pow&apos;r Divine&quot; showcased Padgett&apos;s powerful baritone and impressive vocal range; a natural actor, he stumbled and hiccupped convincingly through the night&apos;s many entreaties on the importance of good wine without overdoing the gestures.&quot;
             <br />
             <div className='text-right py-3'>Lee Eisman, <a href="https://www.classical-scene.com/2015/06/14/souls-damour-bemf/" target="_blank" rel="noopener noreferrer">The Boston Music Intelligencer</a></div>
           </p>
