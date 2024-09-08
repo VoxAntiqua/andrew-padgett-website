dc8de7ddc62b808113c5ac0cefb94020fd7a4f42
VoxAntiqua <andrew.padgett.88@gmail.com>
2024-09-07 20:31:33 -0400

    Properly centered video cards

--- app/media/video/page.tsx
+++ app/media/video/page.tsx
@@ -4,8 +4,8 @@ import { videos } from '@/app/lib/videoData';
 
 const Video: React.FC = () => {
   return (
-    <div className="container mx-auto p-4">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div className="flex justify-center items-center p-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center">
         {videos.map((video) => (
           <VideoCard key={video.id} video={video} />
         ))}
