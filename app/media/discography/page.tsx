f4dcdabcf8eb77f3db3d62bba907eb9612e132ee
VoxAntiqua <andrew.padgett.88@gmail.com>
2024-09-09 15:11:04 -0400

    AlbumCards within the same row now have the same height

--- app/media/discography/page.tsx
+++ app/media/discography/page.tsx
@@ -5,7 +5,8 @@ export default function Discography() {
   return (
     <div className="p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
       {albums.map((album) => (
-        <div key={album.id} className="aspect-square">
+        <div key={album.id} className="flex">
+          {/* AlbumCard will have a uniform height for all items in a row */}
           <AlbumCard album={album} />
         </div>
       ))}
