eaa2e940c908e08309be47cb219001300455e702
VoxAntiqua <andrew.padgett.88@gmail.com>
2025-09-29 22:38:25 -0400

    Adjusted splash image

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,32 @@
-import Image from "next/image"; // Import Image from Next.js
+import Image from "next/image";
 
 export default function Home() {
   return (
     <section className="relative w-full h-screen">
-      {/* Splash Image */}
+      {/* Desktop Splash Image */}
       <Image
-        src="/images/SplashSeated.jpg"
+        src="/images/SplashSeatedCropped.jpg"
         alt="Home Splash"
-        fill  // Fill the entire viewport
+        fill
+        className="hidden sm:block" // hidden on mobile, visible on sm+
         style={{ 
-          objectFit: 'cover',
-          objectPosition: 'top'  // <- This ensures the image crops only at the bottom
-        }}        priority  // Prioritize loading this image
+          objectFit: "cover",
+          objectPosition: "top",
+        }}
+        priority
+      />
+
+      {/* Mobile Splash Image */}
+      <Image
+        src="/images/SplashSeatedMobile.jpg"
+        alt="Home Splash Mobile"
+        fill
+        className="block sm:hidden" // visible on mobile, hidden on sm+
+        style={{ 
+          objectFit: "cover",
+          objectPosition: "top",
+        }}
+        priority
       />
     </section>
   );
